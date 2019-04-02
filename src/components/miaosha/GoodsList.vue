e72aca68e0bfe664b3a08fef3c32a170b6ecad4e
dailiang2017 <346238190@qq.com>
2019-04-02 13:22:11 +0800

    提交

--- src/components/miaosha/GoodsList.vue
+++ src/components/miaosha/GoodsList.vue
@@ -1,7 +1,7 @@
 <template>
   <section>
     <el-row :gutter="15">
-      <el-col :span="4" v-for="goods in goodList">
+      <el-col :span="4" v-for="goods in goodList" :key="goods.id">
         <el-card :body-style="{ padding: '0px' }">
           <img v-bind:src="goods.goodsImg" class="image">
           <div style="padding: 14px;">
@@ -14,39 +14,13 @@
               </div>
             </div>
             <div align="center" style="margin-top: 10px">
-              <el-button type="danger" style="width: 100%;" @click="createVerifyCode(goods.id)">秒杀</el-button>
+              <el-button type="danger" style="width: 100%;" @click="">秒杀</el-button>
             </div>
           </div>
         </el-card>
       </el-col>
     </el-row>
-    <el-dialog title="请证明你不是机器人" :visible.sync="verifyVisible"  id="verifyForm">
 
-      <el-form :inline="true"  class="demo-form-inline" :model="verifyForm">
-        <el-form-item label="">
-          <img v-bind:src="verifyCodeSrc" style="width: 200px;height: 50px">
-        </el-form-item>
-        <el-form-item label="">
-          <el-input v-model="verifyForm.verifyCode"></el-input>
-        </el-form-item>
-        <el-form-item>
-          <el-button type="primary" @click="verifySubmit()">确定</el-button>
-        </el-form-item>
-      </el-form>
-
-    </el-dialog>
-    <el-dialog title="正在排队中" :visible.sync="paiduVisible">
-      <div role="progressbar" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"
-           class="el-progress el-progress--line el-progress--text-inside">
-        <div class="el-progress-bar">
-          <div class="el-progress-bar__outer" style="height: 18px;">
-            <div class="el-progress-bar__inner" :style="{width: processData+'%'}">
-              <div class="el-progress-bar__innerText">{{processData}}%</div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </el-dialog>
   </section>
 </template>
 <script>
@@ -66,72 +40,13 @@
       };
     },
     methods: {
-      createVerifyCode(seckillGoodsId) {
-        this.$http.get('/api/user/getUser').then((response) => {
+      queryList() {
+        this.$http.get('/goods/queryList').then((response) => {
           let data = response.data;
-          if (data.code === 0) {
-            this.verifyCodeSrc = "/api/kill/verifyCode?seckillGoodsId=" + seckillGoodsId + "&currentTime=" + Date.now()
-            this.verifyVisible = true
-            this.verifyForm.seckillGoodsId = seckillGoodsId
-          }else {
-            this.$message(data.msg);
-          }
-        });
-      },
-      verifySubmit() {
-        let path = '/api/kill/path?seckillGoodsId=' + this.verifyForm.seckillGoodsId + "&verifyCode=" + this.verifyForm.verifyCode
-        this.$http.get(path).then((response) => {
-          let data = response.data;
-          if (data.code === 0) {
-            let miaoshaPath = data.data;
-            this.verifyVisible = false
-            this.openPaiDui(miaoshaPath)
-          } else {
-            this.$message(data.msg);
-            this.createVerifyCode(this.verifyForm.seckillGoodsId)
-          }
-        });
-        this.verifyForm.verifyCode = ''
-      },
-      kill(miaoshaPath) {
-        let path = '/api/kill/' + miaoshaPath + '/miaosha?seckillGoodsId=' + this.verifyForm.seckillGoodsId
-        this.$http.get(path).then((response) => {
-          let data = response.data;
-          if (data.code === 0) {
-            //入队列了
-          } else {
-            this.killResult = data.msg;
-          }
-        });
-      },
-
-      queryResult() {
-        //已经秒杀过了
-        if (this.killResult) {
-          this.$message(this.killResult);
-        } else {
-          let path = '/api/order/queryResult?seckillGoodsId=' + this.verifyForm.seckillGoodsId
-          this.$http.get(path).then((response) => {
-            let data = response.data;
-            if (data.code === 0) {
-              let orderId = data.data
-              // alert(orderId)
-              this.$router.push({path: '/orderList', query: {'id': orderId}});
-            } else {
-              this.$message(data.msg);
-            }
-          });
-        }
-        this.killResult = ''
-        this.paiduVisible = false
-      },
-      getData() {
-        this.$http.post('/api/goods/queryList').then((response) => {
-          let data = response.data;
-          if (data.code === 0) {
+          if (data.success === true) {
             let list = data.data;
             for (let goods of list) {
-              goods.goodsImg = "../../../static/pic/" + goods.goodsImg;
+              goods.goodsImg = "../../../static/pic/goods/" + goods.goodsImg;
             }
             this.goodList = list
           } else {
@@ -139,26 +54,9 @@
           }
         });
       },
-      openPaiDui(miaoshaPath) {
-        //请求秒杀
-        this.kill(miaoshaPath);
-
-        this.processData = 0
-        this.paiduVisible = true
-        let self = this;
-        let interval = setInterval(getTotelNumber, 1000)
-        function getTotelNumber() {
-          self.processData += 20
-          if (self.processData === 100) {
-            clearInterval(interval);
-            //查询结果
-            self.queryResult(miaoshaPath)
-          }
-        }
-      }
     },
     created() {
-      this.getData();
+      this.queryList()
     }
   }
 </script>
@@ -166,7 +64,7 @@
   .time {
     font-size: 13px;
     color: #999;
-  }
+  }s
 
   .bottom {
     margin-top: 13px;
